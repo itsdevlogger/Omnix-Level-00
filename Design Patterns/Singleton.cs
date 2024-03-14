537b6dd9bc7775a2f8075047a999635cbcc4be6e
itsdevlogger <itsdevlogger@users.noreply.github.com>
2024-03-14 19:11:39 +0530

    - Added Singleton - Added Buttons Row for editor

--- Design Patterns/Singleton.cs
+++ Design Patterns/Singleton.cs
@@ -19,4 +19,33 @@ namespace Omnix.DesignPatterns
             }
         }
     }
+    
+    public abstract class SingletonNeverNull<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+
+                _instance = new GameObject($"[{typeof(T).Name}]").AddComponent<T>();
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
 }
