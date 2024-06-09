4ed03777b4b5fd79c7f11872964925139236f77c
Vaishnav Chincholkar <61238534+thevaishnav@users.noreply.github.com>
2024-06-09 17:51:18 +0530

    Added Scene Managemet

---
+++ Scene Management/SceneIdExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Omnix.SceneManagement
+{
+    public static class SceneIdExtensions
+    {
+        
+    }
+}
---
+++ Scene Management/SceneProperties.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace Omnix.SceneManagement
+{
+    [System.Serializable]
+    public class SceneProperties
+    {
+        public SceneId scene;
+        public LoadSceneMode mode;
+        public bool isAsync;
+
+        public SceneProperties(string name)
+        {
+            scene = name.GetId();
+            mode = LoadSceneMode.Single;
+            isAsync = true;
+        }
+        
+        public SceneProperties(SceneId scene)
+        {
+            this.scene = scene;
+            mode = LoadSceneMode.Single;
+            isAsync = true;
+        }
+
+        public void Load() => scene.Load(mode, isAsync);
+    }
+}
